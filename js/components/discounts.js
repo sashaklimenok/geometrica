a58676935066f1bcd76b0fa301456bbcfaf9ba17
IT-Academy-Gomel <IT-Academy-Gomel>
2022-07-19 21:33:17 +0300

    add slider

---
+++ js/components/discounts.js
@@ -0,0 +1,37 @@
+new Swiper('.discounts__swiper', {
+    // Optional parameters
+    loop: true,
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 20,
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
