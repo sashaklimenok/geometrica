9929c5a0ab6d626ecdd77f95b182de7eed1ce6e6
IT-Academy-Gomel <IT-Academy-Gomel>
2022-07-22 20:43:22 +0300

    add yandex map

--- js/components/discounts.js
+++ js/components/discounts.js
@@ -3,7 +3,7 @@ new Swiper('.discounts__swiper', {
     loop: true,
     direction: 'horizontal',
     slidesPerView: 3,
-    spaceBetween: 20,
+    spaceBetween: 40,
 
     breakpoints: {
         // when window width is >= 320px
@@ -18,20 +18,48 @@ new Swiper('.discounts__swiper', {
         },
         // when window width is >= 640px
         640: {
-            slidesPerView: 4,
+            slidesPerView: 3,
             spaceBetween: 40
         }
-    },
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
     },
 
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+
+});
+
+new Swiper('.discounts__swiper-2', {
+    // Optional parameters
+    loop: true,
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 40,
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next-2',
+        prevEl: '.swiper-button-prev-2',
     },
 
 });
